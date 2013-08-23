319c0f703a99aa8709141163251967a224eed799
igor <igor@igorsnote>
2013-08-23 16:31:33 +0400

    sencha tests

---
+++ tests/SenchaTests/extjsinit.js
@@ -0,0 +1,6 @@
+(function(){
+document.write('<link rel="stylesheet" type="text/css" href="file:///E:\\work3\\ext-4.2.1.883\\resources\\css\\ext-all-debug.css"></link>');
+document.write('<script type="text/javascript" src="file://E:\\work3\\ext-4.2.1.883\\ext-all-debug.js"></script>');
+//document.write('<script type="text/javascript" src="file:///E:\\work3\\ext-4.2.1.883\\ext-all.js"></script>');
+}
+)();
